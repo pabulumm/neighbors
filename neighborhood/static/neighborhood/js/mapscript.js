2635fec21aeb148309decca9ef74331b4bdf668e
Tucker Stone <tuckerstone16@gmail.com>
2016-06-12 17:13:40 -0700

    Change map center, display title tweaks

--- neighborhood/static/neighborhood/js/mapscript.js
+++ neighborhood/static/neighborhood/js/mapscript.js
@@ -116,7 +116,7 @@ var map = L.mapbox.map('map', 'mapbox.streets', {
         position: 'topleft'
     },
     zoomControl: false
-}).setView([37.676670, -119.418438], 16);
+}).setView([37.627556, -118.982436], 16);
 
 // move zoom control location to top right corner
 new L.Control.Zoom({position: 'topright'}).addTo(map);
